13202a62f2a108e666365c1da1ebf7ad77ef4b8d
Waqas Shah <35991182+waqassshah16@users.noreply.github.com>
2021-05-28 02:44:04 +0500

    javascript assignment added
    new javascript assignment is added

---
+++ pages/javascriptAssignmentSaylani/table/table.js
@@ -0,0 +1,9 @@
+var table=Number( prompt("Please insert any Number for Table")); 
+
+document.write("<h2>" +"This is table of "+""+""+table   ); 
+for(var i=1 ; i<=15 ;  i++){
+document.write("<h1>" + "<hr>" +table + "  "+" X " + i + "  " +" = " +i*table +"<br>" ); 
+
+
+
+}
