9c0b6b7e2ae9ca74e8b17329af9840497746b5dc
Waqas Shah <35991182+waqassshah16@users.noreply.github.com>
2021-06-27 02:59:09 +0500

    table program
    table program some changes

--- pages/javascriptAssignmentSaylani/table/table.js
+++ pages/javascriptAssignmentSaylani/table/table.js
@@ -3,6 +3,6 @@ var table=Number( prompt("Please insert any Number for Table"));
 document.write("<h2>" +"This is table of "+""+""+table   +"<br>"); 
 for(var i=1 ; i<=10 ;  i++){
 
-    document.write("<br>"+table +"X" + i + "=" + (table*i) ); 
+    document.write("<br> "+table +" X " + i + " = "  + " " + (table*i) ); 
    
 }
