e4ceee2bc5625cbfff696bf4291dddca0c7b8166
Waqas Shah <35991182+waqassshah16@users.noreply.github.com>
2021-06-27 01:46:49 +0500

    zakat calc
    no update

--- pages/javascriptAssignmentSaylani/zakatcalc/zakcalc.js
+++ pages/javascriptAssignmentSaylani/zakatcalc/zakcalc.js
@@ -16,7 +16,10 @@ document.write("This is total Amount Of Your all Things " + " Rs: " + totalAmoun
 
 if (totalAmount >=(99917*7.5)){
      document.write(yourName + "  "+  "Janab Aap ki Tamam Amount mila kr Zakat ka Nisaab Banti hai ap pr Zakat Farz hai " ); 
- } 
+ var thisIsYourZakat=totalAmount/40; 
+document.write("<hr> "+"<br>"); 
+document.write("Janab "+"  " +yourName+" Aap ki Tamam Tr Amount Pr Itni Zakat "+"  " + "Rs:"+thisIsYourZakat +" "+"Banti Hai "); 
+    } 
 else{
     document.write("janab "+"  "+ yourName + "Aap Zakat Denay k Ahel Nahin hain aap ki tamam amount Zakat k Nisaab k Baraber nahin hai .")
  }
